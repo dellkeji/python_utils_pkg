071a47b67d475e349c55d9c069551cf69e37d7d5
bellkeyang <bellkeyang@tencent.com>
2024-09-06 11:04:04 +0800

    feat: add datatime obj to str and timestamp

--- tests/test_time_transfer.py
+++ tests/test_time_transfer.py
@@ -6,24 +6,36 @@ from utils.time_transfer import TimeTransfer
 
 
 @pytest.mark.parametrize(
-    "input_time, format, expected_time", 
+    "input_time, format, expected_time",
     [
-        ("2024-09-04", "%Y-%m-%d", datetime(2024, 9, 4)), 
+        ("2024-09-04", "%Y-%m-%d", datetime(2024, 9, 4)),
         ("2024-09-04 12:30:45", "%Y-%m-%d %H:%M:%S", datetime(2024, 9, 4, 12, 30, 45)),
         ("2024-09-04 12:30", "%Y-%m-%d %H:%M", datetime(2024, 9, 4, 12, 30)),
-    ]
+    ],
 )
 def test_time_transfer_success(input_time, format, expected_time):
     assert TimeTransfer.str_to_datetime(input_time, format) == expected_time
 
 
 @pytest.mark.parametrize(
-    "input_time, format, expected_time", 
+    "input_time, format, expected_time",
     [
-        ("2024-09-04", "%Y-%m-%d %H", datetime(2024, 9, 4)), 
+        ("2024-09-04", "%Y-%m-%d %H", datetime(2024, 9, 4)),
         ("2024-09-04 12:30:45", "%Y-%m-%d", datetime(2024, 9, 4, 12, 30, 45)),
-    ]
+    ],
 )
 def test_time_transfer_fail(input_time, format, expected_time):
     with pytest.raises(ValueError):
         TimeTransfer.str_to_datetime(input_time, format)
+
+
+@pytest.mark.parametrize(
+    "input_time, format, expected_time",
+    [
+        (datetime(2024, 9, 6, 10, 28), "%Y-%m-%d %H:%M", "2024-09-06 10:28"),
+        (datetime(2024, 9, 6, 10, 28, 30), "%Y-%m-%d %H:%M:%S", "2024-09-06 10:28:30"),
+        (datetime(2024, 9, 6), "%Y-%m-%d", "2024-09-06"),
+    ],
+)
+def test_datetime_to_str_success(input_time, format, expected_time):
+    assert TimeTransfer.datetime_to_str(input_time, format) == expected_time
--- utils/time_transfer.py
+++ utils/time_transfer.py
@@ -2,15 +2,66 @@
 time transfer module
 make a util to transfer time format
 """
+
+import json
 from datetime import datetime
 
 
 class TimeTransfer:
-    
+
     @classmethod
     def str_to_datetime(cls, str_time: str, format: str) -> datetime:
-        """str time to datetime"""
+        """str time to datetime
+
+        :param str_time: string time, example: "2021-01-01 12:00:00"
+        :param format: string format, example: "%Y-%m-%d %H:%M:%S"
+        :return: datetime object, example: datetime(2021, 1, 1, 12, 0, 0)
+        """
         try:
             return datetime.strptime(str_time, format)
         except ValueError as e:
             raise ValueError(f"incorrect data format, {e}")
+
+    @classmethod
+    def datetime_to_str(cls, date_time: datetime, format: str) -> str:
+        """datetime to str time
+
+        :param date_time: datetime object, example: datetime(2021, 1, 1, 12, 0, 0)
+        :param format: string format, example: "%Y-%m-%d %H:%M:%S"
+        :return: string time, example: "2021-01-01 12:00:00"
+        """
+        try:
+            return date_time.strftime(format)
+        except ValueError as e:
+            raise ValueError(f"incorrect data format, {e}")
+        except TypeError as e:
+            raise TypeError(f"incorrect data type, {e}")
+
+    @classmethod
+    def str_to_timestamp(cls, str_time: str) -> int:
+        """str time to timestamp
+
+        :param str_time: string time, example: "2021-01-01 12:00:00"
+        :return: timestamp
+        """
+        supported_formats = [
+            "%Y-%m-%d",
+            "%Y-%m-%d %H",
+            "%Y-%m-%d %H:%M",
+            "%Y-%m-%d %H:%M:%S",
+            "%Y-%m-%d %H:%M:%S.%f",
+            "%Y/%m/%d",
+            "%Y/%m/%d %H",
+            "%Y/%m/%d %H:%M",
+            "%Y/%m/%d %H:%M:%S",
+            "%Y/%m/%d %H:%M:%S.%f",
+        ]
+        for format in supported_formats:
+            try:
+                time_obj = cls.str_to_datetime(str_time, format)
+            except ValueError:
+                continue
+            else:
+                return int(time_obj.timestamp())
+        # 如果不符合预期格式，抛出异常
+        raise ValueError(f"incorrect data format, supported formats: {json.dumps(supported_formats)}")
