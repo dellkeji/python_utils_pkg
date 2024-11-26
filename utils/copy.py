7c60cf2b34c277ff84dab787815c418c9a683c1b
dellkeji <tzwydragon@163.com>
2024-11-26 21:51:51 +0800

    feat: init a copy function

--- utils/copy.py
+++ utils/copy.py
@@ -2,3 +2,13 @@
 copy module
 copy files or dir from src to dest
 """
+
+def copy_file(src: str, dst: str) -> bool:
+    """复制文件
+    分下面几种场景
+    - 源和目的都是文件
+    - 源是文件，目的是目录
+    - 源是目录，目的也是目录
+    """
+    pass
+    
