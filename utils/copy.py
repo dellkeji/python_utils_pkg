56f74446d3d5ebce057160a8c7f17ddfd121e529
dellkeji <tzwydragon@163.com>
2024-12-15 10:21:19 +0800

    feat: add src check type for copy function

--- utils/copy.py
+++ utils/copy.py
@@ -2,6 +2,8 @@
 copy module
 copy files or dir from src to dest
 """
+from pathlib import Path
+
 
 def copy_file(src: str, dst: str) -> bool:
     """复制文件
@@ -10,5 +12,18 @@ def copy_file(src: str, dst: str) -> bool:
     - 源是文件，目的是目录
     - 源是目录，目的也是目录
     """
+    # 检查源是文件还是目录
     pass
+
+
+def _is_file(src: str) -> bool:
+    """判断是否是文件"""
+    obj = Path(src)
+    return obj.is_file()
+
+
+def _is_dir(src: str) -> bool:
+    """判断是否是目录"""
+    obj = Path(src)
+    return obj.is_dir()
     
