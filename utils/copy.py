a286db739c3d4f95e58f2dda9dd8ef2ad3e78c27
dellkeji <tzwydragon@163.com>
2024-12-29 13:54:07 +0800

    feat: add copy file to dst file or dir

--- utils/copy.py
+++ utils/copy.py
@@ -2,18 +2,41 @@
 copy module
 copy files or dir from src to dest
 """
+import os
+import shutil
+from typing import Optional
+
 from pathlib import Path
 
 
-def copy_file(src: str, dst: str) -> bool:
+def copy_file(src: str, dst: str, is_dst_file: Optional[bool] = False) -> bool:
     """复制文件
     分下面几种场景
     - 源和目的都是文件
     - 源是文件，目的是目录
     - 源是目录，目的也是目录
     """
+    # 检查原路径存在
+    if not os.path.exists(src):
+        # TODO: 可以记录日志或者直接抛出异常
+        return False
     # 检查源是文件还是目录
+
+
+def _copy_dir(src: str, dst: str) -> bool:
+    """拷贝目录下的内容到指定的目录"""
     pass
+
+
+def _copy_file(src: str, dst: str, is_dst_file: Optional[bool] = False) -> bool:
+    """拷贝文件到指定位置"""
+    dst_path = Path(dst)
+    # 检测目录是否存在，不存在，则创建
+    if not os.path.exists(dst):
+        dst_path.parent.mkdir(parents=True, exist_ok=True)
+    # 拷贝文件
+    shutil.copy(src, dst)
+    return True
 
 
 def _is_file(src: str) -> bool:
