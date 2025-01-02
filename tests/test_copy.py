2ada7cacd4441025d27129bde3de1827c30e7324
dellkeji <tzwydragon@163.com>
2025-01-02 21:57:20 +0800

    feat: add test case

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -12,9 +12,13 @@ def create_and_delete_file():
     src_path = "/tmp/test/test.txt"
     with open(src_path, "w") as f:
         f.write("test")
+    src_path1 = "/tmp/test/test1.txt"
+    with open(src_path1, "w") as f:
+        f.write("test1")
     # 删除文件
     yield
     os.remove(src_path)
+    os.remove(src_path1)
 
 
 def test_copy_file(create_and_delete_file):
@@ -23,3 +27,8 @@ def test_copy_file(create_and_delete_file):
     copy_file(src_path, dst_path)
 
     assert os.path.exists(dst_path+"/test.txt")
+
+    src_path = "/tmp/test/test1.txt"
+    dst_path = "/tmp/test1/test1.txt"
+    copy_file(src_path, dst_path, is_dst_file=True)
+    assert os.path.exists(dst_path)
